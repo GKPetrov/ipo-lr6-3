7ce31f8292e33e22cf718f8e875d969e7f54bc14
Gleb <glebpetrof2007@gmail.com>
2024-12-04 16:26:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-num=int(input("Введите кол-во строк"))
-str_list=[]
-new_list=[]
+num = int(input("Введите кол-во строк"))
+str_list = []
+new_list = []
 if(num<0):
     print("error")
 else:
     for i in range(num):
         str_list.append(input("Введите строку"))
     for i in range(num):
-            str_list[i]=str_list[i].split(" ")
+            str_list[i] = str_list[i].split(" ")
     for i in range(num):
         new_list.extend(str_list[i])
     print(len(set(new_list)))
